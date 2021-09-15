df93e7a13a66df42646100961446143ef8df11ff
mnesheretov <90177587+mnesheretov@users.noreply.github.com>
2021-09-15 13:15:29 +0300

    Add files via upload

---
+++ Source — копия.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+    char a;
+	unsigned char b;
+	short c;
+	unsigned short d;
+	int e;
+	unsigned f;
+	long g;
+	unsigned long h;
+	float i;
+	double j;
+	long double k;
+
+	char a1=1;
+	unsigned char b1=2;
+	short c1=3;
+	unsigned short d1=4;
+	int e1=50;
+	unsigned f1=500;
+	long g1=1000;
+	unsigned long h1 = 20000;
+	float i1 = 7;
+	double j1 = 500;
+	long double k1 = 100;
+
+	printf("%d \n", a1);
+	printf("%d \n", b1);
+	printf("%d \n", c1);
+	printf("%d \n", d1);
+	printf("%d \n", e1);
+	printf("%d \n", f1);
+	printf("%d \n", g1);
+	printf("%d \n", h1);
+	printf("%f \n", i1);
+	printf("%lf \n", j1);
+	printf("%lf \n", k1);
+		return 0;
+}
+
+
